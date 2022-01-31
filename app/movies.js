efcbd5441d2e51281334798fd77602f9ae14ff6f
Leonardo Segars <lsegars@gmail.com>
2022-01-31 10:53:18 -0300

    Its working

--- app/movies.js
+++ app/movies.js
@@ -4,9 +4,47 @@ var router = express.Router();
 
 const movies = require('./sample.json');
 
+const _ = require("underscore");
+
 //Expose routers
-router.get('/movies', function (req, res) {
+router.get('/', (req, res) => {
     res.json({ movies: movies })
+    
+})
+
+router.post('/', (req, res) => 
+{
+    const {title, director, year, rating} = req.body;
+    
+    if (!(title && director && year && rating)) {
+        return res.status(400).json('Wrong answer');
+    }
+
+    // let movie = {
+    //     "id": movies.length + 1,
+    //     "title": title,
+    //     "director":director,
+    //     "year": year,
+    //     "rating":rating
+    // };
+    var id= movies.length + 1;
+    var movie = {id, ...req.body};
+    movies.push(movie);
+    res.json('saved');
+
+    //res.json({ movies: movies })
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    _.each(movies, (movie, i)=>{
+        if (movie.id == id) {
+            movies.splice(i,id);
+        }
+        return;
+    });
+
+    res.json('delete');
 })
 
 module.exports = router;
--- app/server.js
+++ app/server.js
@@ -4,18 +4,21 @@ const router = require('./routes');
 const movierouter = require('./movies');
 
 var app = express();
-
-app.use('/api', [router, movierouter]);
-
 //middlewares
 app.use(morgan('combined'));
 //for json interpretation
-app.use(express.urlencoded({extended: true}))
 app.use(express.json(
     {
         limit: '5mb'
     }
-));
+));     
+app.use(express.urlencoded({extended: true}))
+
+app.use('/api', router);
+app.use('/api/movies', movierouter);
+
+
+
 
 //servicio de la nube process.env.port sino el por defecto el 4800
 var port = process.env.port  || 4800; 
