39316623a2bc876e1629df10e4108a67d06624b3
Leo <lsegars@gmail.com>
2022-04-12 15:58:51 -0300

    Feature/typescript (#5)
    * Main tu type (#4)
    
    * Cambio de sample to js and add DockerFile
    
    * New readme file
    
    Co-authored-by: Leonardo <leonardo@finalis.com>
    
    * update sample
    
    * Last Test
    
    * Main tu type (#4)
    
    * Cambio de sample to js and add DockerFile
    
    * New readme file
    
    Co-authored-by: Leonardo <leonardo@finalis.com>
    
    * update sample
    
    Co-authored-by: Leonardo <leonardo@finalis.com>

--- src/sample.js
+++ src/sample.js
@@ -11,7 +11,7 @@ function getSample() {
     
         },
         {
-            "id": "1",
+            "id": "7",
             "title": "The Dark Knight",
             "director":"Christopher Nolan",
             "year": "2008",
